b4f0fb7ae222c5dfca4a2011669817c32063454f
fynnbehnke <fynn.behnke@gmail.com>
2022-12-20 23:47:21 +0100

    Added LiDAR and Odometry Server to according Ports

--- apr_sim/src/lidar_server.cpp
+++ apr_sim/src/lidar_server.cpp
@@ -1,104 +1,106 @@
-// create a TCP/echo_server (in C++) on the turtlebot and test it with port 10000 TCP/echo_client (in C++) on your PC
-#include <stdio.h>      /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket(), bind(), and connect() */
-#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <string.h>
-#include <unistd.h>     
+#include <unistd.h>
 #include <iostream>
 
-#define RCVBUFSIZE 32   /* Size of receive buffer */
-#define MAXPENDING 5    /* Maximum outstanding connection requests */
+#define MAXPENDING 5
 
-// compile with:    gcc tcp_echo_server.cpp -lpthread -o server
-// run with :       ./server 10000
-
-/* Error handling function */
 void DieWithError(std::string errorMessage)
 {
     std::cout << errorMessage << std::endl;
     exit(1);
 }
 
-void HandleTCPClient(int clntSocket)
-{
-    char const* echoBuffer = "---START---{\"header\": {\"seq\": 11809, \"stamp\": {\"secs\": 1667556711, \"nsecs\": 341204004}, \"frame_id\": \"odom\"}, \"child_frame_id\": \"base_footprint\", \"pose\": {\"pose\": {\"position\": {\"x\": 1.9796332120895386, \"y\": -0.32911765575408936, \"z\": 0.0}, \"orientation\": {\"x\": 0.0, \"y\": 0.0, \"z\": -0.09343531727790833, \"w\": 0.995625376701355}}, \"covariance\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}, \"twist\": {\"twist\": {\"linear\": {\"x\": 0.0, \"y\": 0.0, \"z\": 0.0}, \"angular\": {\"x\": 0.0, \"y\": 0.0, \"z\": -0.002709658583626151}}, \"covariance\": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}}___END___";        /* Buffer for echo string */
-    int receiveMessageSize = strlen(echoBuffer);
-    int counter = 0;                    /* Size of received message */
+class LidarPub{
+    public:
+        LidarPub(){
+            lidar_sub = nh.subscribe("/scan", 1, &LidarPub::scan_cb, this);
 
-    /* Receive message from client */
-    //if ((receiveMessageSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
-    //    DieWithError("recv() failed");
+            if ((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+                std::cout << "socket() failed" << std::endl;
+            
+            memset(&echoServAddr, 0, sizeof(echoServAddr));
+            echoServAddr.sin_family = AF_INET;
+            echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+            echoServAddr.sin_port = htons(echoServPort);
 
-    /* Send received string and receive again until end of transmission */
-    while (counter < 10)      /* zero indicates end of transmission */
-    {
-        std::cout << echoBuffer << std::endl;
-        /* Echo message back to client */
-        if (send(clntSocket, echoBuffer, receiveMessageSize, 0) != receiveMessageSize)
-            DieWithError("send() failed");
+            if (bind(serverSocket, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+                std::cout << "bind() failed" << std::endl;
 
-        /* See if there is more data to receive */
-        //if ((receiveMessageSize = recv(clntSocket, &echoBuffer, RCVBUFSIZE, 0)) < 0)
-        //    DieWithError("recv() failed");
+            if (listen(serverSocket, MAXPENDING) < 0)
+                std::cout << "listen() failed" << std::endl;
 
-        counter++;
-        sleep(1);
-    }
+            clntLen = sizeof(echoClntAddr);
 
-    close(clntSocket);    /* Close client socket */
-}
+            if ((clientSocket = accept(serverSocket, (sockaddr*) &echoClntAddr, &clntLen)) < 0)
+                std::cout << "accept() failed" << std::endl;
 
+            std::cout << "Handling client " << inet_ntoa(echoClntAddr.sin_addr) << std::endl;
+        }
+
+        ~LidarPub(){
+            close(clientSocket);
+        }
+
+        void scan_cb(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+            std::stringstream port_msg;
+
+
+            port_msg << "---START---{\"header\": {\"seq\": " << scan_msg->header.seq << ", \"stamp\": {\"secs\": " << scan_msg->header.stamp.sec << ", \"nsecs\": " << scan_msg->header.stamp.nsec << "}, \"frame_id\": \"" << scan_msg->header.frame_id << "\"}, ";
+            port_msg << "\"angle_min\": " << scan_msg->angle_min << ", \"angle_max\": " << scan_msg->angle_max << ", \"angle_increment\": " << scan_msg->angle_increment << ", \"time_increment\": " << scan_msg->time_increment << ", \"scan_time\": " << scan_msg->scan_time << ", \"range_min\": " << scan_msg->range_min << ", \"range_max\": " << scan_msg->range_max << ", \"ranges\": [";
+
+            for(int i = 0; i < scan_msg->ranges.size(); i++){
+                if(i == scan_msg->ranges.size()-1){
+                    port_msg << scan_msg->ranges.at(i) << "], \"intensities\": [";
+                }else{
+                    port_msg << scan_msg->ranges.at(i) << ", ";
+                }
+            }
+
+            for(int i = 0; i < scan_msg->intensities.size(); i++){
+                if(i == scan_msg->intensities.size()-1){
+                    port_msg << scan_msg->intensities.at(i) << "]}___END___";
+                }else{
+                    port_msg << scan_msg->intensities.at(i) << ", ";
+                }
+            }
+
+            message_string = port_msg.str();
+            message = message_string.c_str();
+            message_length = strlen(message);
+
+            if (send(clientSocket, message, message_length, 0) != message_length)
+                DieWithError("send() failed");
+
+        }
+
+    private:
+        // ROS Stuff
+        ros::NodeHandle nh;
+        ros::Subscriber lidar_sub;
+
+        // TCP_Stuff
+        int serverSocket;
+        int clientSocket;
+        struct sockaddr_in echoServAddr;
+        struct sockaddr_in echoClntAddr;
+        unsigned short echoServPort = 9997;
+        unsigned int clntLen;
+
+        std::string message_string;
+        char const* message;
+        int message_length;
+};
 
 int main(int argc, char *argv[])
 {
-    int serverSocket;                       /* Socket descriptor for server */
-    int clientSocket;                       /* Socket descriptor for client */
-    struct sockaddr_in echoServerAddress;   /* Local address */
-    struct sockaddr_in echoClientAddress;   /* Client address */
-    unsigned short echoServerPort;          /* Server port */
-    unsigned int clientLength;              /* Length of client address data structure */
+    ros::init(argc, argv, "Lidar_Server_Node");
+    LidarPub LidarObject;
 
-    if (argc != 2)     /* Test for correct number of arguments */
-    {
-        std::cout << "Usage:  " << argv[0] << " <Server Port>" << std::endl;;
-        exit(1);
-    }
+    ros::spin();
 
-    echoServerPort = std::stoi(argv[1]);  /* First arg:  local port */
-
-    /* Create socket for incoming connections */
-    if ((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        std::cout << "socket() failed" << std::endl;
-      
-    /* Construct local address structure */
-    memset(&echoServerAddress, 0, sizeof(echoServerAddress));   /* Zero out structure */
-    echoServerAddress.sin_family = AF_INET;                     /* Internet address family */
-    echoServerAddress.sin_addr.s_addr = htonl(INADDR_ANY);      /* Any incoming interface */
-    echoServerAddress.sin_port = htons(echoServerPort);         /* Local port */
-
-    /* Bind to the local address */
-    if (bind(serverSocket, (sockaddr *) &echoServerAddress, sizeof(echoServerAddress)) < 0)
-        std::cout << "bind() failed" << std::endl;
-
-    
-    /* Listening for a client to connect. */
-    if (listen(serverSocket, MAXPENDING) < 0)
-        std::cout << "listen() failed" << std::endl;
-
-    /* Waiting for a client to connect. */
-    while(1){
-        /* Set the size of the in-out parameter */
-        clientLength = sizeof(echoClientAddress);
-
-        /* Wait for a client to connect */
-        if ((clientSocket = accept(serverSocket, (sockaddr *) &echoClientAddress, &clientLength)) < 0)
-            std::cout << "accept() failed" << std::endl;
-
-        /* clntSock is connected to a client! */
-
-        /* It prints the IP address of the client. */
-        std::cout << "Handling client " << inet_ntoa(echoClientAddress.sin_addr) << std::endl;
-
-        HandleTCPClient(clientSocket);
-    }
+    return 0;
 }
---
+++ apr_sim/src/odometry_server.cpp
@@ -0,0 +1,109 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+#define MAXPENDING 5
+
+void DieWithError(std::string errorMessage)
+{
+    std::cout << errorMessage << std::endl;
+    exit(1);
+}
+
+class OdometryPub{
+    public:
+        OdometryPub(){
+            odom_sub = nh.subscribe("/odom", 1, &OdometryPub::odom_cb, this);
+
+            if ((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+                std::cout << "socket() failed" << std::endl;
+            
+            memset(&echoServAddr, 0, sizeof(echoServAddr));
+            echoServAddr.sin_family = AF_INET;
+            echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+            echoServAddr.sin_port = htons(echoServPort);
+
+            if (bind(serverSocket, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+                std::cout << "bind() failed" << std::endl;
+
+            if (listen(serverSocket, MAXPENDING) < 0)
+                std::cout << "listen() failed" << std::endl;
+
+            clntLen = sizeof(echoClntAddr);
+
+            if ((clientSocket = accept(serverSocket, (sockaddr*) &echoClntAddr, &clntLen)) < 0)
+                std::cout << "accept() failed" << std::endl;
+
+            std::cout << "Handling client " << inet_ntoa(echoClntAddr.sin_addr) << std::endl;
+        }
+
+        ~OdometryPub(){
+            close(clientSocket);
+        }
+
+        void odom_cb(const nav_msgs::Odometry::ConstPtr& odom_msg){
+            std::stringstream port_msg;
+
+
+            port_msg << "---START---{\"header\": {\"seq\": " << odom_msg->header.seq << ", \"stamp\": {\"secs\": " << odom_msg->header.stamp.sec << ", \"nsecs\": " << odom_msg->header.stamp.nsec << "}, \"frame_id\": \"" << odom_msg->header.frame_id << "\", \"child_frame_id\": \"" << odom_msg->child_frame_id << "\", \"pose\": {\"pose\": {\"position\": ";
+            port_msg << "{\"position\": {\"x\": " << odom_msg->pose.pose.position.x << ", \"y\": " << odom_msg->pose.pose.position.y << ", \"z\": " << odom_msg->pose.pose.position.z << "}, \"orientation\": {\"x\": " << odom_msg->pose.pose.orientation.x << ", \"y\": " << odom_msg->pose.pose.orientation.y << ", \"z\": " << odom_msg->pose.pose.orientation.z << ", \"w\": " << odom_msg->pose.pose.orientation.w << "}}, \"covariance\": [";
+
+            for(int i = 0; i < odom_msg->pose.covariance.size(); i++){
+                if(i == odom_msg->pose.covariance.size()-1){
+                    port_msg << odom_msg->pose.covariance.at(i) << "]}, ";
+                }else{
+                    port_msg << odom_msg->pose.covariance.at(i) << ", ";
+                }
+            }
+
+            port_msg << "{\"twist\": {\"twist\": {\"linear\": {\"x\": " << odom_msg->twist.twist.linear.x << ", \"y\": " << odom_msg->twist.twist.linear.y << ", \"z\": " << odom_msg->twist.twist.linear.z << "}, \"angular\": {\"x\": " << odom_msg->twist.twist.angular.x << ", \"y\": " << odom_msg->twist.twist.angular.y << ", \"z\": " << odom_msg->twist.twist.angular.z << "}}, \"covariance\": [";
+
+            for(int i = 0; i < odom_msg->twist.covariance.size(); i++){
+                if(i == odom_msg->twist.covariance.size()-1){
+                    port_msg << odom_msg->twist.covariance.at(i) << "]}}___END___";
+                }else{
+                    port_msg << odom_msg->twist.covariance.at(i) << ", ";
+                }
+            }
+
+
+            message_string = port_msg.str();
+            message = message_string.c_str();
+            message_length = strlen(message);
+
+            if (send(clientSocket, message, message_length, 0) != message_length)
+                DieWithError("send() failed");
+
+        }
+
+    private:
+        // ROS Stuff
+        ros::NodeHandle nh;
+        ros::Subscriber odom_sub;
+
+        // TCP_Stuff
+        int serverSocket;
+        int clientSocket;
+        struct sockaddr_in echoServAddr;
+        struct sockaddr_in echoClntAddr;
+        unsigned short echoServPort = 9998;
+        unsigned int clntLen;
+
+        std::string message_string;
+        char const* message;
+        int message_length;
+};
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "Odometry_Server_Node");
+    OdometryPub OdometryObject;
+
+    ros::spin();
+
+    return 0;
+}
